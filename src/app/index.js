3b396019f6e9d0bb0bc0f57046434eda9ce3aea6
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-02 10:58:49 -0400

    finished lessons1-3

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('It works!!!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+class TodoComponent extends React.Component{
+    render() {
+        return(
+            <h1>Hello ninjas!</h1>
+        );
+    }
+}
+
+// Put component into index.html
+
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
