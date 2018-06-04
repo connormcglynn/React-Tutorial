5075717c18d210731e8473b8b0411f9bdc4ad361
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-04 10:28:15 -0400

    finished lesson 7

--- src/app/index.js
+++ src/app/index.js
@@ -13,23 +13,30 @@ class TodoComponent extends React.Component{
         var todos = this.state.todos;
         todos = todos.map(function(item, index){
             return(
-                <li>{item}</li>
+                <TodoItem item={item} key={index}/>
             );
         });
         return(
             <div id ="todo-list">
                 <p>The busiest people have the most leisure...</p>
-                <p>{this.state.age}</p>
-                <ul>
-                    {todos}
-                </ul>
+                <ul>{todos}</ul>
             </div>
         );
     }// render
+};
+
+//Create TodoItem component
+class TodoItem extends React.Component{
+    render(){
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                </div>
+            </li>
+        )
+    }
 }
 
-
-
 // Put component into index.html
-
 ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
