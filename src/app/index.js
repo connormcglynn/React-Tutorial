740c375674af5c8a11b3f5e7b82f2101a1bf1238
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-03 22:45:45 -0400

    finished lesson 6

--- src/app/index.js
+++ src/app/index.js
@@ -6,24 +6,22 @@ class TodoComponent extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            todos: ['wash up', 'eat some cheese', 'take a nap'],
-            age: 31
+            todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers']
         }
     }
     render() {
-        var ager = setTimeout(function(){
-            this.setState({
-                age: 35
-            });
-        }.bind(this), 5000);
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(
+                <li>{item}</li>
+            );
+        });
         return(
             <div id ="todo-list">
                 <p>The busiest people have the most leisure...</p>
                 <p>{this.state.age}</p>
                 <ul>
-                    <li>{this.state.todos[0]}</li>
-                    <li>{this.state.todos[1]}</li>
-                    <li>{this.state.todos[2]}</li>
+                    {todos}
                 </ul>
             </div>
         );
