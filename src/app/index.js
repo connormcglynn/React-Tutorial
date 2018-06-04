a6e5c9ea3551ce677535c85024447388ff5b9782
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-04 13:43:19 -0400

    finished lesson 8

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ var ReactDOM = require('react-dom');
 class TodoComponent extends React.Component{
     constructor(props){
         super(props)
+        this.onDelete = this.onDelete.bind(this);
         this.state = {
             todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers']
         }
@@ -13,9 +14,9 @@ class TodoComponent extends React.Component{
         var todos = this.state.todos;
         todos = todos.map(function(item, index){
             return(
-                <TodoItem item={item} key={index}/>
+                <TodoItem item={item} key={index} onDelete={this.onDelete}/>
             );
-        });
+        }.bind(this));
         return(
             <div id ="todo-list">
                 <p>The busiest people have the most leisure...</p>
@@ -23,20 +24,40 @@ class TodoComponent extends React.Component{
             </div>
         );
     }// render
+
+    //Custom functions
+    onDelete(item) {
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !== val;
+        });
+        this.setState({
+            todos: updatedTodos
+        });
+    }
 };
 
 //Create TodoItem component
 class TodoItem extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleDelete = this.handleDelete.bind(this);
+    }
     render(){
         return(
             <li>
                 <div className="todo-item">
                     <span className="item-name">{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}> [x]</span>
                 </div>
             </li>
         )
     }
-}
+
+    //Custom functions
+    handleDelete() {
+        this.props.onDelete(this.props.item);
+    }
+};
 
 // Put component into index.html
 ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
